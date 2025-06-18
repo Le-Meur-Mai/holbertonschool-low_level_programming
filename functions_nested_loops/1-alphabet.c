76e7b6757a7abf506c2edb6096e7a03ecb59edbf
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-18 15:19:20 +0000

    7

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+_putchar(i) = 'a';
+
+	do {
+		_putchar(i);
+		i++;
+	} while (i <= 'z');
+
+		_putchar('\n');
+
+	return (0);
+	}
