aeeaf0b51e2a93254de45f206545cb56a0af3795
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-18 08:23:09 +0000

    11

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+
+	if (i != 'q' && i != 'e')
+	{
+		putchar(i);
+		i++;
+	}
+	else
+	{
+		i++;
+	}
+	}
+	putchar('\n');
+	return (0);
+}
