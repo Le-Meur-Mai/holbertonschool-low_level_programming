30b07f58ef2d0c224875084178e35446e5897a38
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-19 13:11:54 +0000

    13

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+
+{
+	int i;
+
+	do {
+		print_alphabet();
+		_putchar('\n');
+		i++;
+	} while (i <= 10);
+	}
