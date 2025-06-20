25d4f06ec01916ff87d38b3d2840346177ca617d
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-20 12:58:27 +0000

    17

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
-
-
-
+/**
+ * add- add a and b
+ * @b:The other number we add.
+ * @a: The number we add.
+ * add- add a and b
+ * Return: result
+ */
 
 int add(int a, int b)
 {
-	int result = ((a+b));
+	int result = ((a + b));
 	{
 		return (result);
 	}
