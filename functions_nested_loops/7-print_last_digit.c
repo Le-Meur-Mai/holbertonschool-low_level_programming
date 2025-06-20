fbc4ab080cf89688f86b0ca223e915e6ba2c6fd6
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-20 12:03:44 +0000

    16

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit- print
+ * @r: Whats have to be checked.
+ * absolute_value : read the name
+ * Return: The absolute value of 'n'
+ */
+
+int print_last_digit(int r)
+
+{
+	return ((r % 10));
+}
