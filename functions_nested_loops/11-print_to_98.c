5f3f438befc32c433b08a47affd7bd5fe0715be6
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-20 13:34:12 +0000

    16

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 void print_to_98(int n)
 
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 	do {
 	printf("%d, ", n);
@@ -18,10 +18,9 @@ void print_to_98(int n)
 	}
 	else
 	{
-		do {
-			printf("%d, ", n);
-			n--;
-		} while (n > 98);
+	while (n > 98);
+	{
+		printf("%d, ", n);
 	}
 	printf("98");
 	printf("\n");
