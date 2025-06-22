a8ddce83c016c8239058ae0d233d0aea29949404
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-22 16:48:23 +0000

    16

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,34 +1,43 @@
 #include "main.h"
+/**
+  * jack_bauer- Print hour
+  * jack bauer: Print hour
+  * dizaine_heure: [0]0:00
+  * heure: 0[0]:00
+  * dizaine_minute: 00:[0]0
+  * minute: 00:0[0]
+  **/
 
 
 
 void jack_bauer(void)
 
 {
-	unsigned int dizaine_heure;
-	unsigned int heure;
-	unsigned int dizaine_minute;
-	unsigned int minute;
+	int dizaine_heure;
+	int heure;
+	int dizaine_minute;
+	int minute;
 
-	while (dizaine_heure <= 2 && heure <= 4 && dizaine_minute <= 5 && minute <= 9)
-	{
 		for (dizaine_heure = 0 ; dizaine_heure <= 2 ; dizaine_heure++)
 		{
-			for (heure = 0 ; heure <= 4 ; heure++)
+			for (heure = 0 ; heure <= 9 ; heure++)
 			{
+				if (dizaine_heure == 2 && heure == 4  && dizaine_minute > 5 && minute > 9)
+				{
+					break;
+				}
 				for (dizaine_minute = 0 ; dizaine_minute <= 5 ; dizaine_minute++)
 				{
 					for (minute = 0 ; minute <= 9 ; minute++)
 					{
-						_putchar(dizaine_heure);
-							_putchar(heure);
-							_putchar(':');
-							_putchar(dizaine_minute);
-							_putchar(minute);
-							_putchar('\n');
+						_putchar(dizaine_heure + '0');
+						_putchar(heure + '0');
+						_putchar(':');
+						_putchar(dizaine_minute + '0');
+						_putchar(minute + '0');
+						_putchar('\n');
 					}
 				}
 			}
 		}
-	}
 }
