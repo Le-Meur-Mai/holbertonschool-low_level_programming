17f4bbde2ebcd64edc6475676eb8f9404a5b2042
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-22 19:12:06 +0000

    23

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+  * times_table- table de multiplication
+  *times_table: table de multiplication
+  * a: nombre principal de la table
+  * b: autre nombre de 1 a 9
+  * result: resultat de a * b
+  **/
 
 
 
@@ -8,44 +14,37 @@ void times_table(void)
 	int a = 0;
 	int b = 0;
 	int result = a * b;
-	int fin_result;
-	int debut_result;
 
 	for (a = 0 ; a <= 9 ; a++)
 	{
 	for (b = 0 ; b <= 9 ; b++)
 	{
 		result = a * b;
-
 		if (result <= 9 && b != 9)
 		{
 			_putchar(result + '0');
 			_putchar(',');
 			_putchar(' ');
-			if ( (b + 1) * a < 10)
+			if ((b + 1) * a < 10)
 			{
 			_putchar(' ');
 			}
 		}
-		else if (b != 9)
-		{
-			debut_result = result / 10;
-			fin_result = result % 10;
-		_putchar(debut_result + '0');
-		_putchar(fin_result + '0');
-		_putchar(',');
-		_putchar(' ');
-		}
-		else if (result ==9 || (result < 9 && b == 9))
+		else if (result == 9 || (result < 9 && b == 9))
 		{
 			_putchar(result + '0');
 		}
+		else if (b != 9)
+		{
+		_putchar((result / 10) + '0');
+		_putchar((result % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		}
 		else
 		{
-			debut_result = result / 10;
-			fin_result = result % 10;
-			_putchar(debut_result + '0');
-			_putchar(fin_result + '0');
+			_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 		}
 	}
 	_putchar('\n');
