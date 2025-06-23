49908bf20d6ab9aaba26c59caa61020d829fada0
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-23 09:48:49 +0000

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  * print_line- read the name
+  * print_line: print the number of lines desired
+  * @n: number of lines
+  * nb_line: do a loop that repeat _
   **/
 
 void print_line(int n)
@@ -16,11 +16,11 @@ void print_line(int n)
 
 	for (nb_line = 0 ; nb_line <= n ; nb_line++)
 	{
-			_putchar('_');
+		_putchar('_');
 	}
 	_putchar('\n');
 	}
-	else if ( n <= 0)
+	else if (n <= 0)
 	{
 		_putchar('\n');
 	}
