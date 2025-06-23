0ae1797abd61969f93ce218d1cbd817910e05e43
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-23 14:27:46 +0000

    14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
-  *
+  * more_numbers- print numbers from 1 to 14
+  * more_numbers: print numbers from 1 to 14
+  * a: numbers
+  * b: lines
   **/
 
 void more_numbers(void)
 
 {
 	int a;
+	int b;
 
-	for ( a = 0 ; a <= 10 ; a++)
+	for (b = 0 ; b < 10 ; b++)
 	{
-
+		for (a = 0 ; a <= 14 ; a++)
+		{
+			if (a >= 10)
+			{
+				_putchar((a / 10) + '0');
+			}
+			_putchar((a % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
