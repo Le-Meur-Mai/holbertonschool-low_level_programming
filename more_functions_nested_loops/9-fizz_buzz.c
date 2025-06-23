046fcb5c0ae9279d780d39e961f6931616a90ed1
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-23 12:37:27 +0000

    12

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main- An Holberton exercise
+  * nb: numbers
+  * main: start of the program, print 1 to 100 with Fizz and Buzz
+  * Return: int
+  **/
+
+int main(void)
+
+{
+	int nb;
+
+	for (nb = 1 ; nb <= 100 ; nb++)
+	{
+		if ((nb % 3) == 0 && (nb % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((nb % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((nb % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (nb == 100)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+		printf("%d ", nb);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
