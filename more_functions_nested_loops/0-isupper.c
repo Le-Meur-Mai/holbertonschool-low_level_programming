cd194a5c9ac6a69d590215fcab98b3d663edefef
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-23 08:07:05 +0000

    1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+/**
+  * _isupper- if its a capital letter
+  * _isupper: if it's a capital letter
+  * @c: the letter to change
+  * Return: 1 if its capital letter, 0 else
+  **/
+int _isupper(int c)
+
+{
+	if isupper(c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
