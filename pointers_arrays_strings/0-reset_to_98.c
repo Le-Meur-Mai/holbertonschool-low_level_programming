85a6c021d8e11313f403d6da9f47b8f02538a330
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-25 08:18:36 +0000

    1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * reset_to_98- Reset n to 98
+  * reset_to_98: Reset n to 98
+  * @n: pointer, its the number to reset
+  **/
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
