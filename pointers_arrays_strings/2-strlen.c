548d26e4de9a4f96236a5a4002c23d4dcd7329e6
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-25 08:56:47 +0000

    3

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strlen- count the lenght of a string
+  * _strlen: count the lenght of a string
+  * @s: Pointer of a char
+  * debut: where the counting is started at the beggining of the string
+  * Return: The value of the numbers of characters
+  **/
+
+int _strlen(char *s)
+
+{
+	int debut = 1;
+
+	for (debut = 1; s[debut] ; debut++)
+	{
+	}
+	return (debut);
+}
