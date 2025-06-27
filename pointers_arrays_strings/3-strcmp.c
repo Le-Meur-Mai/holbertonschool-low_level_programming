20dff9b8d96f2c64cfeb6bbae6817b575d6c8faa
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 21:35:11 +0000

    add_new_conditions

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,22 +12,40 @@
 int _strcmp(char *s1, char *s2)
 
 {
+	int ls1 = 0;
+	int ls2 = 0;
 	int i;
 	int difference = 0;
 
+	for (; s1[ls1] ; ls1++)
+	{
+	}
+	for (; s2[ls2] ; ls2++)
+	{
+	}
 		for (i = 0 ; s1[i] != '\0' ; i++)
 		{
 			if (s1[i] == s2[i])
 			{
 			return (0);
 			}
-			else if (s1[i] > s2[i])
+			else if (s1[i] > s2[i] || s1[i] < s2[i])
 			{
 			return (s1[i] - s2[i]);
 			}
-			else if (s1[i] < s2[i])
+		}
+		if (ls2 > ls1)
+		{
+			for (; s2[ls1] ; ls1++)
 			{
-			return (s1[i] - s2[i]);
+				return (s1[ls1] - s2[ls1]);
+			}
+		}
+		if (ls1 > ls2)
+		{
+			for (; s1[ls2] ; ls2++)
+			{
+				return (s1[ls2] - s2[ls2]);
 			}
 		}
 	return (difference);
