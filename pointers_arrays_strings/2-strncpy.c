16485ec8d1a97d4dbf722a725b70be939d440760
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 09:27:19 +0000

    code verification

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+  * _strncpy- copy the number of characters of the string desired
+  * _strncpy- copy the number of characters of the string desired
+  * @dest: string of destination
+  * @src: string of origin
+  * @n: numbers of characters desired to copy
+  * copy: use to copy from a string to another
+  * i: counting
+  * Return: destination
+  **/
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int copy;
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+	}
+
+	if (i >= n)
+	{
+		for (copy = 0 ; copy <= n ; copy++)
+		{
+		dest[copy] = src[copy];
+		}
+	}
+	else if (i < n)
+	{
+		for (copy = 0 ; copy <= i ; copy++)
+		{
+			dest[copy] = src[copy];
+		}
+		for (; copy != n ; copy++)
+		{
+			dest[copy] = 0;
+		}
+	}
+
+	return (dest);
+}
