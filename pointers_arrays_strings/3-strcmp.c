f92581329ebaa239908334708c768287398d610c
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 12:44:03 +0000

    correction

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,22 +11,22 @@ int _strcmp(char *s1, char *s2)
 
 {
 	int i;
-	int difference;
+	int difference = 0;
 
-	for (i = 0 ; s1[i] ; i++)
-	{
-		if (s1[i] == s2[i])
+		for (i = 0 ; s1[i] != '\0' ; i++)
 		{
-			difference = difference + '0';
+			if (s1[i] == s2[i])
+			{
+			return (0);
+			}
+			else if (s1[i] > s2[i])
+			{
+			return (s1[i] - s2[i]);
+			}
+			else if (s1[i] < s2[i])
+			{
+			return (s1[i] - s2[i]);
+			}
 		}
-		else if (s1[i] != s2[i] && s1[i] > s2[i])
-		{
-			difference = difference - '1';
-		}
-		else if (s1[i] != s2[i] && s1[i] < s2[i])
-		{
-			difference = difference + '1';
-		}
-	}
 	return (difference);
 }
