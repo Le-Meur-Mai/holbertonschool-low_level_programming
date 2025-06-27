20e5f4c0c778e1578c05d05ee3fcba90be6da604
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 21:54:55 +0000

    test_to_see_if_the_code_is_working

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,17 +13,15 @@ int _strcmp(char *s1, char *s2)
 
 {
 	int i;
+	int resultat = 0;
 
-		for (i = 0 ; s1[i] != '\0' ; i++)
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] != s2[i])
-			{
+			resultat = s1[i] - s2[i];
 			break;
-			}
-			else
-			{
-				return (0);
-			}
 		}
-	return (s1[i] - s2[i]);
+	}
+	return (resultat);
 }
