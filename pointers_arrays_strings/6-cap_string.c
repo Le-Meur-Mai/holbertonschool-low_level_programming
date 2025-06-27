5f8bb38961247920a6a06dc2d7812c4d3ad7bbea
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 18:03:34 +0000

    save_progress

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,3 +7,29 @@
   *
   *
   **/
+
+char *cap_string(char *str)
+
+{
+	int i = 0;
+	char separator[] = { ' ', '	', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0' };
+	int j;
+	int k;
+
+	for (; str[i] ; i++)
+	{
+		for (j = 0 ; separator[j] ; j++)
+		{
+			if (str[i] == separator[j])
+			{
+				k = i;
+
+				for (; str[k] >= 'a' && str[k] <= 'z' ; k++)
+				{
+				}
+				str[k] = str[k] - 32;
+			}
+		}
+	}
+	return (str);
+}
