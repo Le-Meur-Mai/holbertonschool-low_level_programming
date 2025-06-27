c53154c79bae679715b64509609025ba8d3a2996
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-27 08:45:11 +0000

    add comments

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
-  *
-  *
-  *
-  *
+  * _strcat- merge two strings on the string dest
+  * _strcat: merge two strings : dest + src
+  * @dest: string of destination
+  * @src: string to add to dest
+  * dest_length: length of the dest string
+  * start: variable for counting
+  * Return: string dest
   **/
 
 char *_strcat(char *dest, char *src)
