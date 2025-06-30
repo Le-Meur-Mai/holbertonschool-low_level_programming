828259d06b26c108ad0809b13731f0c7f0e7e73d
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-30 11:15:28 +0000

    add an if condition

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,6 +14,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	unsigned int j = 0;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0 ; src[i] != '\0' && i < n ; i++)
 	{
 		dest[j] = src[i];
