82990f687c1444047c8153f71e51fb245522dc19
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-30 16:39:04 +0000

    code verification

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,43 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  * _strstr- function that locate a substring
+  * _strstr: locate the substring needle is haystack
+  * @haystack: string you want to search in
+  * @needle: string you want to find in haystack
+  * Return: (adress of the substring found
   **/
 
 char *_strstr(char *haystack, char *needle)
 
 {
+	int i = 0;
+	int j = 0;
+	int length_of_needle = 0;
+	char *start;
 
+	for (; needle[length_of_needle] ; length_of_needle++)
+	{
+	}
+	length_of_needle = length_of_needle - 1;
+
+	for (; haystack[i] ; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			start = &haystack[i];
+			for (j = 0; needle[j] != haystack[i] || j != length_of_needle ; j++)
+			{
+				i++;
+			}
+			if (j == length_of_needle)
+			{
+				break;
+			}
+		}
+	}
+	if (haystack[i] == '\0')
+	{
+		return (0);
+	}
+	return (start);
+}
