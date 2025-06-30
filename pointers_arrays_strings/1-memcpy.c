fd9086d9cbb660ea656ba9fc9cec9ad6b4fee763
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-30 11:19:12 +0000

    delete some elements

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,16 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
 	unsigned int i;
-	unsigned int j = 0;
 
-	if (n == 0)
+	for (i = 0 ; i < n ; i++)
 	{
-		return (0);
-	}
-	for (i = 0 ; src[i] != '\0' && i < n ; i++)
-	{
-		dest[j] = src[i];
-		j++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
