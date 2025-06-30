aa1451b0baf8e98f11f3b35bc7b46390e68b4dd0
Le-Meur-Mai <mai.lm@outlook.fr>
2025-06-30 09:23:09 +0000

    code verification

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _memset- replace n bytes by char b
+  * _memset: replace n bytes by char b
+  * @s : string of characters
+  * @b : character you want to print at the beginning of the string
+  * @n : numbers of bytes you want to replace
+  * Return: s
+  **/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i = 0;
+
+	for (; i <= n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
