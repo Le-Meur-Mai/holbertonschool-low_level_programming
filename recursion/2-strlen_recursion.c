b34b207c0f80377722a3e56838d27c2c5c088411
Le-Meur-Mai <mai.lm@outlook.fr>
2025-07-02 08:02:45 +0000

    function that print the length of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
   *
   *
   *
   *
+  *
   **/
+
 int _strlen_recursion(char *s)
 
 {
-	int i = 0;
-
 	if (*s == '\0')
 	{
-		return (i);
+		return (0);
 	}
-	_strlen_recursion(s + 1);
+
+	return (1 + _strlen_recursion(s + 1));
 }
