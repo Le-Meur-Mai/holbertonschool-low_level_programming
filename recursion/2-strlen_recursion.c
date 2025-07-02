736259b005335331662b6574debedb0fbd21051f
Le-Meur-Mai <mai.lm@outlook.fr>
2025-07-02 08:06:57 +0000

    Add description

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
-  *
+  * _strlen_recursion- print the length of a string
+  * _strlen_recursion: print the length of a string
+  * @s: string you want to work with
+  * Return: the length of the string
   **/
 
 int _strlen_recursion(char *s)
