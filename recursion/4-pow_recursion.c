52fdc995f12aeaacfd9442ed035069627549bec9
Le-Meur-Mai <mai.lm@outlook.fr>
2025-07-02 12:08:58 +0000

    add comments

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  * _pow_recursion- print the result of x power y
+  * _pow_recursion: print the result of x power y
+  * @x: number you want to multiply with himself
+  * @y: number that depicts the power of x
+  * Return: the result of x power y
   **/
 
 int _pow_recursion(int x, int y)
