30f922c37907fc0c55a1469049d120c4a159ccf3
root <root@DESKTOP-0BUHC3U.localdomain>
2025-07-07 14:45:24 +0200

    function verification

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup- duplicate a string with dynamic allocation
+ * _strdup: duplicate string with dynamic allocation
+ * @str: string to duplicate
+ * Return: NULL if str is NULL, or the pointer to the duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	char *copy = NULL;
+	int i = 0;
+	int j = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+for (; str[i] ; i++)
+{
+}
+
+copy = malloc (sizeof(char) * i);
+
+if (copy == NULL)
+{
+	return (NULL);
+}
+for (; j < i ; j++)
+{
+	copy[j] = str[j];
+}
+return (copy);
+}
