0835565535dc8f106aea4f232a3fcdd92a7ccae3
Le-Meur-Mai <mai.lm@outlook.fr>
2025-07-09 14:05:01 +0200

    create new file dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- struct that contain the name, the age of the dog and his owner
+ * struct dog: struct of all the informations of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
