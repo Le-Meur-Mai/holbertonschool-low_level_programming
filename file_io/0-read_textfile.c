b7820f7855f12d92d9bded8f54785dcda4e92008
Le-Meur-Mai <mai.lm@outlook.fr>
2025-07-31 16:17:42 +0200

    Creation of a function that use system calls to print the wanted numbers of characters from a file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile- Print in the standard output the numbers of characters
+ * wanted from a file, using system calls.
+ * @filename: The file you want your characters printed from.
+ * @letters: Number of characters you want to be printed.
+ * Return: The number of characters read and printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int success = 0;
+	ssize_t counting = 0;
+
+	buffer = malloc(letters + 1);
+
+	if (filename == NULL || letters <= 0)
+		return (0);
+	success = open(filename, O_RDONLY);
+
+	if (success == -1)
+		return (0);
+	counting = read(success, buffer, letters);
+
+	if (counting == -1)
+		return (0);
+
+	success = 0;
+
+	success = write(1, buffer, counting);
+
+	if (success == -1 || success != counting)
+		return (0);
+
+	return (counting);
+}
